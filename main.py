7070d94258a40b317f1b3152c81689407b0a1b3d
Swastik Mukherjee <swastik.reach@gmail.com>
2021-01-05 12:45:22 +0545

    Website modified, text changed to gyanmate

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.sqlite"
 
 @app.route("/")
 def home():
